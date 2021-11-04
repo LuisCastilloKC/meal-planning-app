2823a476a097c1d26e02f9088cd0f9527bf908f0
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-03 23:36:09 -0400

    add get signIn and post signIn to route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'sign_up', to: 'registration#new'
   post 'sign_up', to: 'registrations#create'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create', as: 'log_in'
 
 end
