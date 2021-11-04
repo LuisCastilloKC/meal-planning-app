d0645638bc59fb6c5bc83675f23fb836330c7959
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-03 23:38:04 -0400

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'registrations#create'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create', as: 'log_in'
-
+  delete 'logout', to: 'session#destroy'
 end
