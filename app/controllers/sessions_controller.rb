7c3ce2e8f0da794e3d84173f91cc0cf58c33c1d3
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-04 23:49:29 -0400

    add destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
         end
     end
 
+    def destroy
+        # delete user session
+        session[:user_id] = nil
+        redirect_to root_path, notice: 'Logged Out'
+    end
+
 end
