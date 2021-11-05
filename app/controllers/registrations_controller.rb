d549459c5fc81539dce0ebc3b54f7117c2a567bb
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-04 23:39:39 -0400

    add more action in create and add strong parameters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,26 @@
 class RegistrationsController < ApplicationController
-
+    # instantiates new user
     def new
         @user = usern.new
     end
 
     def create
         @user = User.new(user_params)
+        if @user.save
+            # store save user id in a session
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: 'Successfully created account'
+        else
+            render :new
+        end
     end
+
+    private
+    def user_params
+        #strong parameters
+        params.require(:user).permit(:email, :username, :date_of_birth, :password, :password_confirmation)
+    end
+
+
 
 end
