0d21260605caf63fa08b83d026e635e3f80d5724
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-16 23:42:05 -0500

    create welcome mailer

---
+++ app/mailers/welcome_mailer.rb
@@ -0,0 +1,8 @@
+class WelcomeMailer < ApplicationMailer
+    # sends a welcome email
+    def welcome_email
+        @user = params[:user]
+        @url = 'http://localhost:300/sign_up'
+        mail(to: @user.email. subject: 'Welcome to my mailer)
+    end
+end
