186ad92093724014649dc941c36f6d15f96c8baf
Luis Castillo <mr.luiscastillo@gmail.com>
2021-11-18 23:39:16 -0500

    fix syntax error

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -3,6 +3,6 @@ class WelcomeMailer < ApplicationMailer
     def welcome_email
         @user = params[:user]
         @url = 'http://localhost:300/sign_up'
-        mail(to: @user.email. subject: 'Welcome to my mailer)
+        mail(to: @user.email, subject: 'Welcome to my mailer')
     end
 end
